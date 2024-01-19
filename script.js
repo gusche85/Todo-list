4b876bf69559c17f820f680cf16e5b910ac6e31c
gusche85 <gishjode@gmail.com>
2024-01-19 10:13:56 +0000

    Add header

--- script.js
+++ script.js
@@ -2,21 +2,22 @@ const output = document.getElementById("output");
 
 function showTasks() {
   
-  fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => response.json())
-    .then(listToDo => {
-    const lists = listToDo;
-      let toDoListHTML = "";
-      let listCount = 0;
-      let listNumbers = 50;
-    for (const list of lists) {
-      const toDo = list.title;
-      toDoListHTML += `<div align= center ><br>Task: ${toDo}</div>`;
-      listCount++
-      if (listCount === listNumbers) { break; }
-    }
-        output.innerHTML = toDoListHTML;
-      })
-    .catch(err => console.error(err));
+   fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(listToDo => {
+      const lists = listToDo; 
+        let toDoListHTML = "";
+        let listCount = 0;
+        let listNumbers = 50;
+      for (const list of lists) {
+        const toDo = list.title;
+        const complete = list.completed;
+        const checkMark = complete? '&#9989' : null ;
+        toDoListHTML += `<div align= center >${toDo} ${checkMark}</div>`;
+        listCount++
+        if (listCount === listNumbers) { break; }
+      }
+        output.innerHTML = `<h3 style = "text-align: center; text-decoration: underline">Tasks</h3><div>${toDoListHTML}</div><br><br>`;     
+        })
+  }
 
-}
