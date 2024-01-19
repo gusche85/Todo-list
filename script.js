3c69ea12d2195e477d7c8c85b4451b34ce2475af
gusche85 <gishjode@gmail.com>
2024-01-19 18:04:24 +0000

    Add showList() and completedTasks()

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const output = document.getElementById("output");
+const output1 = document.getElementById("output1");
 
 function showTasks() {
   
@@ -12,10 +13,10 @@ function showTasks() {
         const complete = list.completed;
         const checkMark = complete? '&#9989' : null ;
         toDoListHTML += `<div align= center >${toDo} ${checkMark}</div>`;
-        listCount++
       }
         output.innerHTML = `<h3 style = "text-align: center; text-decoration: underline">Tasks</h3><div>${toDoListHTML}</div><br><br>`;     
         })
+  .catch(err => console.error(err));
   }
 
 function userId() {
@@ -27,7 +28,7 @@ function userId() {
       const uniqueUserIds = Array.from(uniqueId); //convert to array of unique values
 
       let userHTML = `
-        <p style="text-align: center">Select user ID from the dropdown menu</p><br>
+        <h3 style="text-align: center">Select user ID from the dropdown menu</h3><br>
         <select id="userIdDropdown">`;
       uniqueUserIds.forEach(userId => {
         userHTML += `<option value="${userId}">${userId}</option>`;
@@ -38,13 +39,13 @@ function userId() {
 
       output.innerHTML = userHTML;
     })
+  .catch(err => console.error(err));
 }
 
-// Function to display the list based on the selected user ID
 function showList() {
-  const selectedUserId = document.getElementById("userIdDropdown").value;
+  const selectedUser = document.getElementById("userIdDropdown").value;
 
-  fetch(`https://jsonplaceholder.typicode.com/todos?userId=${selectedUserId}`)
+  fetch(`https://jsonplaceholder.typicode.com/todos?userId=${selectedUser}`)
     .then(response => response.json())
     .then(userList => {
       let toDoListHTML = "";
@@ -53,10 +54,28 @@ function showList() {
         const toDo = list.title;
         const complete = list.completed;
         const checkMark = complete ? '&#9989;' : '';
-        toDoListHTML += `<div align=center><br>${toDo} ${checkMark}</div>`;
+        toDoListHTML += `<div align=center>${toDo} ${checkMark}</div>`;
       }
 
-      output.innerHTML += toDoListHTML; // Append the list to the existing content
+      output1.innerHTML = toDoListHTML; // display under output.innerHTML
     })
     .catch(err => console.error(err));
 }
+
+function completedTasks() {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(listToDo => {
+      const lists = listToDo; 
+        let toDoListHTML = "";
+      for (const list of lists) {
+        const toDo = list.title;
+        const complete = list.completed;
+        if(complete) {
+        toDoListHTML += `<div>${toDo}</div>`;}
+      }
+        output.innerHTML = `<h3 style = "text-align: center; text-decoration: underline">These tasks has been completed</h3><div>${toDoListHTML}</div><br><br>`;     
+        })
+  .catch(err => console.error(err));
+  }
+
